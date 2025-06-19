3bcea4f274f87574d045cf4df51098d53931ada0
vaibhavrai53 <nrai96237@gmail.com>
2025-06-20 00:10:10 +0530

    week1_factorymethodpattern
    week1_assignment2

---
+++ week1_patterndesign/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+abstract class Document
+{
+    public abstract void Open();
+}
+
+class WordDocument : Document
+{
+    public override void Open()
+    {
+        Console.WriteLine("Opening a Word document...");
+    }
+}
+
+class PdfDocument : Document
+{
+    public override void Open()
+    {
+        Console.WriteLine("Opening a PDF document...");
+    }
+}
+
+class ExcelDocument : Document
+{
+    public override void Open()
+    {
+        Console.WriteLine("Opening an Excel document...");
+    }
+}
+
+abstract class DocumentFactory
+{
+    public abstract Document CreateDocument();
+}
+
+class WordDocumentFactory : DocumentFactory
+{
+    public override Document CreateDocument()
+    {
+        return new WordDocument();
+    }
+}
+
+class PdfDocumentFactory : DocumentFactory
+{
+    public override Document CreateDocument()
+    {
+        return new PdfDocument();
+    }
+}
+
+class ExcelDocumentFactory : DocumentFactory
+{
+    public override Document CreateDocument()
+    {
+        return new ExcelDocument();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document word = wordFactory.CreateDocument();
+        word.Open();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdf = pdfFactory.CreateDocument();
+        pdf.Open();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excel = excelFactory.CreateDocument();
+        excel.Open();
+    }
+}
